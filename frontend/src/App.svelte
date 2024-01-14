e1e6e9cdbe12fc9ec9d28d214ce1d9fd187e71e3
Arnaud <arnaudclaire20@yahoo.fr>
2024-01-14 15:00:08 +0100

    error and UI management

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -3,35 +3,43 @@
 	let teamX=3;
 	let teamY=3;
 	let possibilities = [];
-	let errorMessage = '';
+	let errorMessage = 'Erreur inattendue';
 
 	const calculatePossibilities = async () => {
-		try {
-			const response = await fetch('http://localhost:3001/af_scores', {
-				method: 'POST',
-				headers: {
-					'Content-Type': 'application/json',
-				},
-				body: JSON.stringify({
-					"teamX": teamX,
-					"teamY": teamY
-				}),
-			});
-
-			if (response.ok) {
-				possibilities = await response.json();
-				errorMessage = '';
-				console.log('Résultats possibles :', possibilities);
-			} else if (response.status === 400) {
-				errorMessage = 'Erreur : Données d\'entrée invalides';
-			} else {
-				console.error('Erreur lors de la requête:', response.statusText);
+		if(teamX < 3 || teamY < 3) {
+			errorMessage = 'Erreur : Les scores doivent être au minimum à 3';
+			return;
+		}
+		else{
+			try {
+				const response = await fetch('http://localhost:3001/af_scores', {
+					method: 'POST',
+					headers: {
+						'Content-Type': 'application/json',
+					},
+					body: JSON.stringify({
+						"teamX": teamX,
+						"teamY": teamY
+					}),
+				});
+				if (response.ok) {
+					possibilities = await response.json();
+					errorMessage = '';
+					console.log('Résultats possibles :', possibilities);
+				} else if (response.status === 400) {
+					errorMessage = 'Erreur : Données d\'entrée invalides';
+				}else if (response.status === 500) {
+					errorMessage = 'Erreur : Le serveur a rencontré un problème, veuillez vérifier les données d\'entrée';
+				}else {
+					console.error('Erreur lors de la requête:', response.statusText);
+					errorMessage = 'Erreur inattendue';
+				}
+			} catch (error) {
+				console.error('Erreur:', error.message);
 				errorMessage = 'Erreur inattendue';
 			}
-		} catch (error) {
-			console.error('Erreur:', error.message);
-			errorMessage = 'Erreur inattendue';
 		}
+
 	};
 </script>
 
@@ -49,14 +57,16 @@
 	</form>
 
 	{#if possibilities.length > 0}
-		<h2>Résultats possibles :</h2>
+	
 		<div class="results">
 			<div class="teamX">
+				<h2>Team X</h2>
 				{#each possibilities[0] as possibilityX}
 					<p>{possibilityX}</p>
 				{/each}
 			</div>
 			<div class="teamY">
+				<h2>Team Y</h2>
 				{#each possibilities[1] as possibilityY}
 					<p>{possibilityY}</p>
 				{/each}
@@ -64,6 +74,8 @@
 		</div>
 	{:else if errorMessage !== ''}
 		<p>{errorMessage}</p>
+	{:else}
+		<p>Waiting...</p>
 	{/if}
 </main>
 
@@ -85,6 +97,13 @@
 	form {
 		margin-top: 40px;
 
+	}
+
+	button {
+		width: 20rem;
+		background-color: rgb(56, 56, 56);
+		color: white;
+		border-radius: 30px;
 	}
 
 	.results {
